e77310c6ae431d3a1971b92732f59959b448d9c7
FoxyXTN <116965272+FoxyXTN@users.noreply.github.com>
2023-05-20 14:39:38 +0200

    Add files via upload

---
+++ Server Cloner Guide By Foxy.py
@@ -0,0 +1,25 @@
+print ("Hey! Welcome To The New Server Cloner Guide Please follow all steps . and dont use it for evil purposes Lets Begin.")
+print ("Please Select one:")
+print ("(Dont Forget The capital Letters)")
+print ("")
+print ("How to use")
+print ("")
+print ("how to stop it")
+print ("")
+print ("type anything else to end the script")
+print ("")
+select = input("type here:")
+
+if select == "How to use":
+
+    print ("")
+    print ("at first u activate the cloner.")
+    print ("then u make a new server and click A u should now see the ISZ Title Screen On your upper screen")
+    print ("now u have To Be Very carefull At first u wanna imagine like theres the main menu. u gotta click on multiplayer. then on any save u want and then go on internet now u should see the server list with your existing server (and maybe others) now u just have to hit host new game and re do the process until. the server list is full with your servers.")
+    print ("Thanks for using this guide. Have Fun!")
+    
+if select == "How to stop it":
+    
+    print ("")
+    print ("If u want to stop the process just Leave the server , go in the server list , and try join your cloned servers it will make an error just hit accept and all the servers will be gone")
+    print ("thanks")
